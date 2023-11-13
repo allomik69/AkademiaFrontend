2b4c43f04d6aece0eac41a5d32ac388c02726338
Juraj <118258305+allomik69@users.noreply.github.com>
2023-11-13 22:22:03 +0100

    Add files via upload

--- 2023lvl1javascript/main.js
+++ 2023lvl1javascript/main.js
@@ -1,72 +1,50 @@
-var winScore = 0;
-var kolo = 0;
+let winScore = 0;
+let round = 0;
+
 function score() {
-    winScore++;
+    winScore++
 }
-document.getElementById("playButton").onclick = function () {
-    if (kolo == 0) {
-        alert("hra sa začína! rozhodni sa , či toto kolo použijieš nožničky , kameň alebo papier.");
-        kolo++;
+
+document.getElementById("playButton").onclick = () => {
+    if (round === 0) {
+        alert("hra sa začína! rozhodni sa, či toto kolo použiješ nožničky, kameň alebo papier.");
+        round++;
     } else {
-        alert("hra pokračuje ! tvoje skóre je: " + winScore);
+        alert("hra pokračuje! tvoje skóre je: " + winScore);
     }
+
     const options = ["nožničky", "kameň", "papier"];
-    var computerChoice = options[Math.floor(Math.random() * 3)];
+    const computerChoice = options[Math.floor(Math.random() * 3)];
     const userChoice = window.prompt("napíš ktorý objekt si si zvolil (K, P alebo N):");
     const validOptions = ["K", "P", "N"];
-    var computerWon = 0;
 
-    if (computerChoice == "nožničky" && userChoice.toUpperCase() == "K") {
-        alert("Vyhral si! Počítač si zvolil: " + computerChoice);
-        score();
-        computerWon = 1;
+    if (!validOptions.includes(userChoice.toUpperCase())) {
+        alert("neplatná hodnota");
+        return;
     }
-    else if (computerChoice == "kameň" && userChoice.toUpperCase() == "P") {
-        alert("Vyhral si! Počítač si zvolil: " + computerChoice);
-        computerWon = 1;
-        score();
-    }
-    else if (computerChoice == "papier" && userChoice.toUpperCase() == "N") {
-        alert("Vyhral si! Počítač si zvolil: " + computerChoice);
-        computerWon = 1;
-        score();
-    }
-    else if (computerChoice == "nožničky" && userChoice.toUpperCase() == "P") {
-        alert("Pehral si! Počítač si zvolil: " + computerChoice)
-        computerWon = 2;
-    }
-    else if (computerChoice == "kameň" && userChoice.toUpperCase() == "N") {
-        alert("Pehral si! Počítač si zvolil: " + computerChoice)
-        computerWon = 2;
-    }
-    else if (computerChoice == "papier" && userChoice.toUpperCase() == "K") {
-        alert("Pehral si! Počítač si zvolil: " + computerChoice)
-        computerWon = 2;
-    }
-    else if (computerChoice == "kameň" && userChoice.toUpperCase() == "K") {
-        alert("remíza ! Počítač si zvolil: " + computerChoice)
-    }
-    else if (computerChoice == "papier" && userChoice.toUpperCase() == "P") {
-        alert("remíza ! Počítač si zvolil: " + computerChoice)
-    }
-    else if (computerChoice == "nožničky" && userChoice.toUpperCase() == "N") {
-        alert("remíza ! Počítač si zvolil: " + computerChoice)
-    }
-    else if (!validOptions.includes(userChoice.toUpperCase())) {
-        alert("neplatná hodnota")
-    }
-
-    console.log("Skóre je: " + winScore);
-
-    if (computerWon == 2) {
-        console.log("kolo vyhral počítač.")
-    }
-    else if (computerWon == 1) {
-        console.log("kolo vyhral hráč.")
-    }
-    else {
-        console.log("bola remíza.")
-    }
-
+    determineWinner(userChoice.toUpperCase(), computerChoice);
 };
-
+const determineWinner = (user, computer) => {
+    let result = "";
+    switch (true) {
+        case (computer === "nožničky" && user === "K"):
+        case (computer === "kameň" && user === "P"):
+        case (computer === "papier" && user === "N"):
+            result = "Vyhral si!";
+            score();
+            break;
+        case (computer === "kameň" && user === "K"):
+        case (computer === "papier" && user === "P"):
+        case (computer === "nožničky" && user === "N"):
+            result = "remíza!";
+            break;
+        case (computer === "papier" && user === "K"):
+        case (computer === "nožničky" && user === "P"):
+        case (computer === "kameň" && user === "N"):
+            result = "Pehral si!";
+            break;
+    }
+    alert(`${result} Počítač si zvolil: ${computer}`);
+    console.log("Skóre je: " + winScore);
+    console.log(result === "Pehral si!" ? "kolo vyhral počítač." : result === "Vyhral si!" ? "kolo vyhral hráč." : "bola remíza.");
+};
