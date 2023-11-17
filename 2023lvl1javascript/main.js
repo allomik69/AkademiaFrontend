927c3d7b12bdc8b44d8d03d5f95a2e5f14f3cf5f
Juraj <118258305+allomik69@users.noreply.github.com>
2023-11-17 21:07:12 +0100

    feedback

--- 2023lvl1javascript/main.js
+++ 2023lvl1javascript/main.js
@@ -1,69 +1,91 @@
-let winScore = 0;
-let round = 0;
-let counter = {};
-counter.k = 0;
-counter.n = 0;
-counter.p = 0;
-
-function score() {
-    winScore++
+let round = 0
+let pcScore = 0
+let userScore = 0
+let counter = {
+    k: 0,
+    n: 0,
+    p: 0
 }
+let history = []
 
 document.getElementById("playButton").onclick = () => {
-    if (round === 0) {
-        alert("hra sa začína! rozhodni sa, či toto kolo použiješ nožničky, kameň alebo papier.");
-        round++;
-    } else {
-        alert("hra pokračuje! tvoje skóre je: " + winScore);
+
+    const options = ["nožničky", "kameň", "papier"]
+    const computerChoice = options[Math.floor(Math.random() * 3)]
+    const validOptions = ["K", "P", "N"]
+
+    if (round === 0) 
+    {
+        alert("hra sa začína! rozhodni sa, či toto kolo použiješ nožničky, kameň alebo papier.")
+        round++
+    } 
+    else 
+    {
+        alert("hra pokračuje! tvoje skóre je: " + userScore)
+        round++
+    }
+    
+    const userChoice = prompt("napíš ktorý objekt si si zvolil (K, P alebo N):")
+
+    if (!validOptions.includes(userChoice.toUpperCase())) 
+    {
+        alert("neplatná hodnota")
+        if ( round <= 2)
+        {
+        round--
+        }
+        return
     }
 
-    const options = ["nožničky", "kameň", "papier"];
-    const computerChoice = options[Math.floor(Math.random() * 3)];
-    const userChoice = window.prompt("napíš ktorý objekt si si zvolil (K, P alebo N):");
-    const validOptions = ["K", "P", "N"];
+    determineWinner(userChoice.toUpperCase(), computerChoice)
+}
 
-    if (!validOptions.includes(userChoice.toUpperCase())) {
-        alert("neplatná hodnota");
-        return;
-    }
-    determineWinner(userChoice.toUpperCase(), computerChoice);
-};
-const determineWinner = (user, computer) => {
-    let result = "";
-    let history = [];
+function determineWinner(user, computer) 
+{
+    let result = ""
 
     switch (true) {
         case (computer === "nožničky" && user === "K"):
         case (computer === "kameň" && user === "P"):
         case (computer === "papier" && user === "N"):
-            result = "Vyhral si!";
-            score();
-            break;
+            result = "Vyhral si!"
+            userScore++
+            break
         case (computer === "kameň" && user === "K"):
         case (computer === "papier" && user === "P"):
         case (computer === "nožničky" && user === "N"):
-            result = "remíza!";
-            break;
+            result = "remíza!"
+            break
         case (computer === "papier" && user === "K"):
         case (computer === "nožničky" && user === "P"):
         case (computer === "kameň" && user === "N"):
-            result = "Pehral si!";
-            break;
+            pcScore++
+            result = "Pehral si!"
+            break
     }
-    if (user === "K") {
-        counter.k++;
-    }
-    else if (user === "N") {
-        counter.n++;
-    }
-    else if (user === "P") {
-        counter.p++;
-    }
-    alert(`${result} Počítač si zvolil: ${computer}`);
-    console.log("Skóre je: " + winScore);
-    console.log(result === "Pehral si!" ? "kolo vyhral počítač." : result === "Vyhral si!" ? "kolo vyhral hráč." : "bola remíza.");
-    history.push(result === "Pehral si!" ? "kolo vyhral počítač." : result === "Vyhral si!" ? "kolo vyhral hráč." : "bola remíza.");
-    console.log(history);
-    console.log(counter);
-};
 
+    if (user === "K") 
+    {
+        counter.k++
+    }
+    else if (user === "N") 
+    {
+        counter.n++
+    }
+    else if (user === "P") 
+    {
+        counter.p++
+    }
+    alert(`${result} Počítač si zvolil: ${computer}`)
+
+    console.clear()
+
+    history.push(result === "Pehral si!" ? " kolo vyhral počítač " : result === "Vyhral si!" ? " kolo vyhral hráč " : " bola remíza ")
+    let displayHistory = history.join('<br>')
+    document.getElementById("displayHistory").innerHTML = displayHistory
+
+    console.log(counter)
+    
+    document.getElementById("displayUserScore").innerHTML = userScore 
+    document.getElementById("displayPcScore").innerHTML = pcScore 
+}
